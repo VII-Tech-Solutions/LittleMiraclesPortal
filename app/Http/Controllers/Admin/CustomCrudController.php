2e12cff8ed611c400d30742800e5a1f6d9081b60
fatema Zuhair <fatema.m2906@gmail.com>
2021-11-28 14:56:16 +0300

    added the custom contoller

--- app/Http/Controllers/Admin/CustomCrudController.php
+++ app/Http/Controllers/Admin/CustomCrudController.php
@@ -50,4 +50,88 @@ class CustomCrudController extends CrudController
         ]);
     }
 
+    /**
+     * Add Description Field
+     * @param string|null $name
+     * @param string|null $label
+     * @param string|null $tab_name
+     * @param string $field_type
+     * @param int $rows
+     * @param integer|array $limit
+     */
+    function addDescriptionField($name = null, $label = null, $tab_name = null, $field_type = FieldTypes::TEXTAREA, $rows = 5, $limit = [], $hint = null)
+    {
+        if (is_null($name)) {
+            $name = Attributes::DESCRIPTION;
+        }
+        if (is_null($label)) {
+            $label = ucwords(Attributes::DESCRIPTION);
+        }
+        if(!is_array($limit)){
+            $limit = [
+                Attributes::MAXLENGTH => $limit
+            ];
+        }
+        CRUD::addField([
+            Attributes::NAME => $name,
+            Attributes::TYPE => $field_type,
+            Attributes::LABEL => ucwords($label),
+            Attributes::ATTRIBUTES => array_merge([
+                Attributes::DIR => Attributes::RTL,
+                Attributes::ROWS => $rows,
+            ], $limit),
+            Attributes::TAB => $tab_name,
+            Attributes::HINT => $hint,
+            'options'       => [
+                Attributes::DIR => Attributes::RTL,
+                "language" => 'ar',
+//                'removePlugins' => 'embed,Embed',
+//                'removeButtons'        => 'Source,Save,Templates,NewPage,ExportPdf,Preview,Print,Cut,Undo,Find,Replace,SelectAll,Scayt,Form,Checkbox,Redo,PasteText,PasteFromWord,About,Maximize,ShowBlocks,BGColor,Styles,TextColor,Format,Font,FontSize,Image,CopyFormatting,NumberedList,Outdent,Blockquote,JustifyLeft,RemoveFormat,Indent,BulletedList,Underline,Strike,Subscript,Superscript,CreateDiv,JustifyCenter,Flash,Table,Anchor,Language,JustifyBlock,JustifyRight,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Italic,Bold',
+            ]
+
+        ]);
+    }
+
+    /**
+     * Add Featured Image Field
+     * @param null $field_name
+     * @param null $label
+     * @param bool $remove_hint
+     * @param null $hint_message
+     * @param null $tab_name
+     */
+    function addFeaturedImageField($field_name = null, $label = null, $remove_hint = false, $hint_message = null, $tab_name = null)
+    {
+        if (is_null($field_name)) {
+            $field_name = Attributes::FEATURED_IMAGE;
+        }
+
+        if (is_null($hint_message)) {
+            $hint_message = "<strong>Note: </strong>Maximum image size is <strong>7MB.</strong>";
+        }
+        CRUD::addField([
+            Attributes::LABEL => is_null($label) ? "Featured Image" : ucwords($label),
+            Attributes::NAME => $field_name,
+            Attributes::TYPE => Attributes::IMAGE,
+            Attributes::HINT => !$remove_hint ? $hint_message : "",
+            Attributes::CROP => false, // set to true to allow cropping, false to disable
+            Attributes::ASPECT_RATIO => 1, // Commit or set to 0 to allow any aspect ratio
+            Attributes::TAB => $tab_name,
+        ]);
+    }
+
+    /**
+     * Add Order Field
+     */
+    function addOrderField($hint = null, $is_disabled = false)
+    {
+        CRUD::addField([
+            Attributes::LABEL => "Order",
+            Attributes::NAME => Attributes::ORDER,
+            Attributes::TYPE => FieldTypes::NUMBER,
+            Attributes::HINT => $hint,
+            Attributes::ATTRIBUTES => $this->disabled($is_disabled),
+        ]);
+    }
+
 }
