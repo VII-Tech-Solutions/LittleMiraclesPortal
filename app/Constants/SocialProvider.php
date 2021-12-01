d6a9c6521f2147babc01d2207fa61d0270373b70
Ahmed Yusuf <ahmed.yusuf@viitech.net>
2021-12-01 13:26:47 +0300

    social provider constants

---
+++ app/Constants/SocialProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Constants;
+
+/**
+ * Social Provider
+ */
+class SocialProvider extends CustomEnum
+{
+    const GOOGLE = "google";
+    const FACEBOOK = "facebook";
+    const SNAPCHAT = "snapchat";
+}
