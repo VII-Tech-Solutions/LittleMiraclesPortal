3cf1d4f320a66191ffe08d03792dd10bd025a752
fatema Zuhair <fatema.m2906@gmail.com>
2021-12-02 11:57:18 +0300

    import the fonts and font awosome

---
+++ resources/views/emails/invoice.blade.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <!-- import shared style -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>*|MC:SUBJECT|*</title>
+    <style type="text/css">
+
+        body{
+            font-family: "Manrope";
+        }
+        .background{
+            background-color: rgb(232 233 235);
+            padding-top: 50pt;
+            height: 1024pt;
+        }
+        .email-content{
+            width: 700pt;
+            height: 800pt;
+            margin-left: auto;
+            margin-right: auto;
+            background-color: rgb(255 255 255);
+            border-radius: 10pt;
+        }
+        .top-container{
+            height: 200pt;
+            background-color: rgb( 187 220 224);
+            display: flex;
+            justify-content: space-between;
+            border-top-right-radius: 10pt;
+            border-top-left-radius: 10pt;
+        }
+        .title-text{
+             font-size: 36pt;
+             color: rgb(69 81 93);
+             line-height: 40pt;
+             letter-spacing: -1pt;
+            font-weight: bold;
+            margin-left: 30pt;
+            margin-top: 61pt;
+            width: 237pt;
+         }
+        .header-title{
+            font-size: 36pt;
+            color: rgb(69 81 93);
+            line-height: 43pt;
+            letter-spacing: -1pt;
+            margin-right: 30pt;
+            margin-top: 79pt;
+        }
+        .bottom-container{
+            display: flex;
+            justify-content: space-between;
+        }
+        .h3{
+            font-size: 16pt;
+            color: rgb(162 168 174);
+            letter-spacing: -0.44pt;
+            margin-top: 39pt;
+            font-weight: bold;
+        }
+        .h1{
+            font-size: 36pt;
+            color: rgb(69 81 93);
+            line-height: 40pt;
+            letter-spacing: -1pt;
+            font-weight: bold;
+            margin-top: 10pt;
+        }
+        .h2{
+            font-size: 18pt;
+            color: rgb(69 81 93);
+            line-height: 25pt;
+            letter-spacing: 0pt;
+            margin-top: 5pt;
+        }
+        .email-container{
+            margin-left: 60pt;
+            margin-right: 60pt;
+        }
+        .h4{
+            font-size: 14pt;
+            color: rgb(69 81 93);
+            line-height: 21pt;
+            letter-spacing: 0pt;
+            margin-top: 13pt;
+        }
+        i{
+            color: #0a0302;
+        }
+        button{
+            border-color: rgb(208 211 214);
+            background-color: rgb(255 255 255);
+            color: rgb(141 196 203);
+            width: 200pt;
+            height: 40pt;
+            border-radius: 30pt;
+        }
+
+        </style>
+    <body>
+    <div class="background">
+        <div class="email-content">
+            <div class="top-container">
+                    <div class="title-text">Little Miracles by Sherin</div>
+                            <div class="header-title">Invoice</div>
+            </div>
+            <div class="email-container">
+            <div class="bottom-container">
+            <div class="h3">Invoice Summary</div>
+                <div class="h3">Total</div>
+            </div>
+            <div class="bottom-container">
+                <div class="h1">Mini Session</div>
+                <div class="h1" style="font-size: 24pt; letter-spacing: -0.67pt">BD80</div>
+            </div>
+                <div class="h2">Monthly Promotion</div>
+                <div class="h2" style="font-weight: bold">08/01/2022</div>
+
+            <div class="bottom-container">
+                <div>
+
+                <div class="h4" style="margin-top: 12pt">
+                    <i style="color: rgb(141 196 203)" class="far fa-calendar-day"></i>
+                    8th, January 2022
+                </div></div>
+                <div>
+                    <button>Add to Calendar</button>
+                </div>
+            </div>
+                <div class="bottom-container">
+                <div class="h4">04:00 PM</div>
+                </div>
+                <div class="h4">1 baby, 2 adult</div>
+                <div class="h4">Pastel Rainbow Backdrop</div>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
