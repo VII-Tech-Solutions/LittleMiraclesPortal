bd5b3740262145f28d99434785fb434a59b17d02
Ahmed Yusuf <ahmed.yusuf@viitech.net>
2021-12-07 16:14:20 +0300

    debug

--- app/API/Controllers/AuthenticationController.php
+++ app/API/Controllers/AuthenticationController.php
@@ -123,6 +123,7 @@ class AuthenticationController extends CustomController
 
             }
         } catch (Exception $e) {
+            dd($e);
             Helpers::captureException($e);
         }
 
