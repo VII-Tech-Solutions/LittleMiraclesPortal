3e2fa1974d2f01999628460e262f85d2d2d06af4
Hussain Sabba <hussain.sabba@viitech.net>
2022-03-03 11:51:44 +0300

    check the provider and provider id for user diffing first other than the ID or Email

--- app/Models/User.php
+++ app/Models/User.php
@@ -200,10 +200,11 @@ class User extends CustomModel implements
     {
         $fields = collect($data);
         $find_by = [Attributes::ID];
-        if(!empty($fields->get(Attributes::EMAIL))){
-            $find_by = [Attributes::EMAIL];
-        }else if(!empty($fields->get(Attributes::PROVIDER)) && !empty($fields->get(Attributes::PROVIDER_ID))){
+         if(!empty($fields->get(Attributes::PROVIDER)) && !empty($fields->get(Attributes::PROVIDER_ID))){
             $find_by = [Attributes::PROVIDER, Attributes::PROVIDER_ID];
+        }
+        else if(!empty($fields->get(Attributes::EMAIL))){
+            $find_by = [Attributes::EMAIL];
         }else if(empty($fields->get(Attributes::ID))) {
             $find_by = [Attributes::COUNTRY_CODE, Attributes::PHONE_NUMBER];
         }
