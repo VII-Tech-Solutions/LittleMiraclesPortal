d43e19379ab250966f18a92dbcc2cfaba0459fb9
Sayed Ali <3alooyalsayed@gmail.com>
2022-03-30 14:39:16 +0300

    remove cakes array slice

--- app/API/Controllers/SessionController.php
+++ app/API/Controllers/SessionController.php
@@ -105,8 +105,8 @@ class SessionController extends CustomController
             $location_link = null;
         }
         $package = Package::findOrfail($package_id);
-        $cakes = array_slice($cakes,-$package->cake_allowed,null,true);
-        $backdrops = array_slice($backdrops,-$package->backdrop_allowed,null,true);
+//        $cakes = array_slice($cakes,-$package->cake_allowed,null,true);
+//        $backdrops = array_slice($backdrops,-$package->backdrop_allowed,null,true);
         // create session
         $session = Session::createOrUpdate([
             Attributes::TITLE => $package->title . " " . $package->tag,
