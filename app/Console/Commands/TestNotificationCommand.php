8ae37651181154b8079c100ce0db956896bde2ba
Tasneem_Ali <tasneem.ali@viitech.net>
2023-09-18 10:39:08 +0300

    test notification

--- app/Console/Commands/TestNotificationCommand.php
+++ app/Console/Commands/TestNotificationCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Helpers\FirebaseHelper;
+use App\Models\Photographer;
 use App\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
@@ -18,13 +19,15 @@ class TestNotificationCommand extends Command
         Log::info("Test Notification");
         Log::info("Env: " . GlobalHelpers::isProductionEnv());
 
-        /** @var User $users */
-        $users = User::whereIn('id', [157, 19])->get();
+        /** @var Photographer $admin_user */
+        $users = Photographer::whereIn('id', [1])->get();
+//        /** @var User $users */
+//        $users = User::whereIn('id', [157, 19])->get();
 
-        /** @var User $user */
+        /** @var Photographer $user */
         foreach ($users as $user) {
-            Log::info("User First Name: " . $user->first_name);
-            $notification = FirebaseHelper::sendFCMByToken($user->device_token, $user->id, null, ['title' => "Happy Birth Day!", 'message' => "It's your BIG DAY! Happy Birthday our Little Miracle  " . $user->first_name . " 🥳🎈"]);
+            Log::info("User First Name: " . $user->name);
+            $notification = FirebaseHelper::sendFCMByToken($user->device_token, $user->id, null, ['title' => "Happy Birth Day!", 'message' => "It's your BIG DAY! Happy Birthday our Little Miracle  " . $user->name . " 🥳🎈"]);
             Log::info($notification);
         }
     }
