48204bc694f8d91921c5eed3dbf77e0bd2717736
Tasneem_Ali <tasneem.ali@viitech.net>
2023-10-11 10:28:42 +0300

    fix birthday notification

--- app/Console/Commands/HappyBirthdayCommand.php
+++ app/Console/Commands/HappyBirthdayCommand.php
@@ -19,13 +19,12 @@ class HappyBirthdayCommand extends Command
     {
         $today_date = Carbon::now()->format('m-d');
         $children = FamilyMember::where('relationship', Relationship::CHILDREN)->get();
-
         /** @var FamilyMember $child */
         foreach ($children as $child) {
             if (Carbon::parse($child->birth_date)->format('m-d') == $today_date) {
                 // get user
                 $parent = $child->user;
-                $notification = FirebaseHelper::sendFCMByToken($parent->device_token, $parent->id, null, ['title' => "Happy Birth Day!", 'message' => "It's your BIG DAY! Happy Birthday our Little Miracle  " . $child->first_name . " 🥳🎈"]);
+                $notification = FirebaseHelper::sendFCMByToken($parent->device_token, $parent->id, null, ['title' => "Happy Birth Day!", 'message' => "It's your BIG DAY! Happy Birthday our Little Miracle " . $child->first_name . " 🥳🎈"]);
             }
         }
     }
